b2532fb9db74e2f2c6758711237b3d580a9411ba
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-19 14:10:47 +0100

    Added the Registration entity with attributes and relations

---
+++ src/main/java/com/sec/eventify/model/Registration.java
@@ -0,0 +1,32 @@
+package com.sec.eventify.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Registration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    private LocalDateTime registeredAt;
+
+    private String status;
+}
+
