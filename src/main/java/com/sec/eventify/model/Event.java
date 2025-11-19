57f749dd95f66f766f1f8f475c2ecd1cdc5c7603
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-19 14:10:02 +0100

    Added the Event entity with attributes and relations

--- src/main/java/com/sec/eventify/model/Event.java
+++ src/main/java/com/sec/eventify/model/Event.java
@@ -1,4 +1,28 @@
 package com.sec.eventify.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String location;
+    private LocalDateTime dateTime;
+    private Long capacity;
+
+    @ManyToOne
+    @JoinColumn(name = "organizer_id", nullable = false)
+    private User organizer;
 }
