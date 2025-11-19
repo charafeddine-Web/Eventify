6f281602b1b71b79b8692ae165d3e358e3948a40
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-19 14:10:15 +0100

    Added the User entity with attributes and relations

--- src/main/java/com/sec/eventify/model/User.java
+++ src/main/java/com/sec/eventify/model/User.java
@@ -1,4 +1,27 @@
 package com.sec.eventify.model;
 
+import com.sec.eventify.model.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 }
