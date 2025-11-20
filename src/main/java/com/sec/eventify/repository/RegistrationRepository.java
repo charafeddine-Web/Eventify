548379da9d6856e5ec9a9dca70100f9f77071128
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-20 11:08:15 +0100

    Ajouter methode findByUserId

--- src/main/java/com/sec/eventify/repository/RegistrationRepository.java
+++ src/main/java/com/sec/eventify/repository/RegistrationRepository.java
@@ -3,5 +3,8 @@ package com.sec.eventify.repository;
 import com.sec.eventify.model.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+    List<Registration> findByUserId(Long userId);
 }
