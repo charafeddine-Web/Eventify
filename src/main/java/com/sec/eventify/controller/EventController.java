9214a7a980588b3bbc05668eb4c5e077346ed2ee
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-11-20 14:50:00 +0000

    extract current user retrieval in organizer controller

--- src/main/java/com/sec/eventify/controller/EventController.java
+++ src/main/java/com/sec/eventify/controller/EventController.java
@@ -1,9 +1,11 @@
 package com.sec.eventify.controller;
 
 import com.sec.eventify.model.Event;
+import com.sec.eventify.security.CustomUserDetails;
 import com.sec.eventify.service.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,19 +16,21 @@ public class EventController {
     private final EventService eventService;
 
     @PostMapping
-    public Event create(@RequestBody Event event) {
-        return eventService.saveEvent(event);
+    public Event create(@RequestBody Event event, Authentication authentication) {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        return eventService.saveEvent(event, userDetails.getUser());
     }
 
     @PutMapping("/{id}")
-    public Event update(@PathVariable Long id, @RequestBody Event event) {
-        event.setId(id);
-        return eventService.upadteEvent(event);
+    public Event update(@PathVariable Long id, @RequestBody Event event, Authentication authentication) {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        return eventService.upadteEvent(event, id, userDetails.getUser());
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        eventService.deleteEvent(id);
+    public void delete(@PathVariable Long id, Authentication authentication) {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        eventService.deleteEvent(id, userDetails.getUser());
     }
 
 }
