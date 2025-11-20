68ebf9734febb9e701ed1bd5f53f6ed917532daf
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-20 11:07:47 +0100

    Modifier le type d'id

--- src/main/java/com/sec/eventify/model/User.java
+++ src/main/java/com/sec/eventify/model/User.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String name;
 
