e38fdcf00fc29693c0971cab843c16a65b8e1c5e
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-21 14:50:00 +0100

    Ajouter la methode deleteEvent

--- src/main/java/com/sec/eventify/controller/AdminController.java
+++ src/main/java/com/sec/eventify/controller/AdminController.java
@@ -1,9 +1,13 @@
 package com.sec.eventify.controller;
 
 import com.sec.eventify.model.User;
+import com.sec.eventify.security.CustomUserDetails;
+import com.sec.eventify.service.EventService;
 import com.sec.eventify.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +18,8 @@ import java.util.List;
 public class AdminController {
     @Autowired
     private final UserService userService;
+    @Autowired
+    private final EventService eventService;
 
     @GetMapping("/users")
     public List<User> getAll() {
@@ -23,6 +29,12 @@ public class AdminController {
     @PutMapping("/users/{id}/role")
     public User changeRole(@PathVariable Long id, @RequestParam String role) {
         return userService.changeRole(id, role);
+    }
+
+    @DeleteMapping("/events/{id}")
+    public void deleteEvent(@PathVariable Long id, Authentication authentication) {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        eventService.deleteEvent(id, userDetails.getUser());
     }
 
 }
