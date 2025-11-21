c6d7a6f0f4a722dd49f19634cd40007c1e1c4024
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-21 10:50:00 +0100

    Ajouter la methode getMyRegistrations

--- src/main/java/com/sec/eventify/controller/RegistrationController.java
+++ src/main/java/com/sec/eventify/controller/RegistrationController.java
@@ -1,16 +1,14 @@
 package com.sec.eventify.controller;
 
 import com.sec.eventify.model.Registration;
-import com.sec.eventify.model.User;
-import com.sec.eventify.model.Event;
-import com.sec.eventify.repository.RegistrationRepository;
-import com.sec.eventify.repository.EventRepository;
-import com.sec.eventify.repository.UserRespository;
 import com.sec.eventify.service.RegistrationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -24,5 +22,13 @@ public class RegistrationController {
                                       @AuthenticationPrincipal UserDetails userDetails) {
 
         return registrationService.registerEvent(id, userDetails.getUsername());
+    }
+
+    @GetMapping("/registrations")
+    public List<Registration> getMyRegistrations(Authentication authentication) {
+
+        String userEmail = authentication.getName();
+
+        return registrationService.getRegistrationsByEmail(userEmail);
     }
 }
