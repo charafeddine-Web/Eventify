101cb8cf6090d1853b161c3302eb447d87f617d2
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-12-16 15:24:32 +0000

    Modifiy EventNotFoundException

--- src/main/java/com/sec/eventify/exception/EventNotFoundExecption.java
+++ src/main/java/com/sec/eventify/exception/EventNotFoundExecption.java
@@ -1,7 +1,7 @@
 package com.sec.eventify.exception;
 
-public class EventNotFoundException extends RuntimeException {
-    public EventNotFoundException(String message) {
+public class EventNotFoundExecption extends RuntimeException {
+    public EventNotFoundExecption(String message) {
         super(message);
     }
 }
